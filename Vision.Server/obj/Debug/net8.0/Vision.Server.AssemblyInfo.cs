b5039ccd429c1ef057c364d9d618fd03a85346f1
danielharder <63276235+danielharder@users.noreply.github.com>
2023-12-10 17:24:22 -0700

    lsakdjgf;

--- Vision.Server/obj/Debug/net8.0/Vision.Server.AssemblyInfo.cs
+++ Vision.Server/obj/Debug/net8.0/Vision.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vision.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d309bf9174cd24ec9160a50bf1cfe017f6746d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vision.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vision.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
